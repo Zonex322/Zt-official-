2e9a93f1bff9113202e948fb1c86ad6da50a5efc
Zonex322 <notzonex115@gmail.com>
2025-05-13 13:04:25 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import random
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+pairs = ["USDINR_OTC", "USDPKR_OTC", "EURUSD", "BTCUSD", "ETHUSD", "USDZAR", "AUDCAD", "EURCAD", "USDCAD", "USDJPY"]
+
+@app.route("/api/signal", methods=["GET"])
+def get_signal():
+    pair = random.choice(pairs)
+    direction = random.choice(["CALL", "PUT"])
+    time = datetime.now(pytz.timezone("Asia/Karachi")).strftime("%H:%M")
+    signal = f"{time} - {pair} - {direction}"
+    return {"signal": signal}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
